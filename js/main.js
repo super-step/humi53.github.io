2ef46825efb7c31c4dd19cfd8f163983b42ab978
humi53 <j.k.humi9@gmail.com>
2023-06-15 16:38:56 +0900

    total

---
+++ js/main.js
@@ -0,0 +1,37 @@
+document.addEventListener("DOMContentLoaded", () => {
+  // todo : 메뉴버튼 마다 링크 달기
+  document.querySelector("nav.cyy_nav")?.addEventListener("click", (e) => {
+    const menu = e.target;
+
+    if (menu.id === "home") {
+      document.location.href = "/index.html";
+    } else if (menu.id === "login") {
+      document.location.href = "/login/login.html";
+    } else if (menu.id === "profile") {
+      document.location.href = "/profile/profile.html";
+    } else if (menu.id === "mypage") {
+      alert(menu.id);
+    }
+  });
+
+  // 과거코드 지워 된다고 함. 혹시몰라서.
+  const gj_map = document.querySelector("area[title='gj_map']");
+  const main_intro = document.querySelector(".main_intro");
+  // main_intro.addEventListener("mouseenter", (main_intro.style.color = "red"));
+});
+
+// if ($('#location-map')) {
+//   $('#location-map area').each(function() {
+//     var id = $(this).attr('id');
+//     $(this).mouseover(function() {
+//       $('#overlay' + id).show();
+
+//     });
+
+//     $(this).mouseout(function() {
+//       var id = $(this).attr('id');
+//       $('#overlay' + id).hide();
+//     });
+
+//   });
+// }
