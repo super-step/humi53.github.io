43a5c50db75bef0a09ea583605fe85084e2173de
humi53 <j.k.humi9@gmail.com>
2023-06-24 17:06:43 +0900

    api_maker_side_title

--- detailmap/API_map.js
+++ detailmap/API_map.js
@@ -24,8 +24,8 @@ var options = {
 // 지도 띄우기
 var map = new kakao.maps.Map(container, options);
 
-/*
-    마커 관련 부분 시작 
+/***************************************
+  마커 관련 부분 시작 
 */
 
 // 마커 좌표 데이터 로드
@@ -34,63 +34,55 @@ let positions = JSON.parse(JSON.stringify(TestFile)).gj;
 var imageSrc =
   "https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/markerStar.png";
 
+let makers = []; // 마커 리스트
+let customOverlays = []; // 커스텀 오버레이 리스트
 for (var i = 0; i < positions.length; i++) {
   // 마커 이미지의 이미지 크기 입니다
   var imageSize = new kakao.maps.Size(24, 35);
   // 마커 이미지를 생성합니다
   var markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize);
+  let position = new kakao.maps.LatLng(
+    positions[i].Latitude,
+    positions[i].longitude
+  );
   // 마커를 생성합니다
-  var marker = new kakao.maps.Marker({
+  let marker = new kakao.maps.Marker({
     map: map, // 마커를 표시할 지도
-    position: new kakao.maps.LatLng(
-      positions[i].Latitude,
-      positions[i].longitude
-    ), // 마커를 표시할 위치
+    position: position, // 마커를 표시할 위치
     image: markerImage, // 마커 이미지
-    title: "test",
+    title: positions[i].name,
     clickable: true,
   });
+  makers.push(marker);
 
-  // 마커에 표시할 인포윈도우를 생성합니다
-  var infowindow = new kakao.maps.InfoWindow({
-    content: `<div id="${positions[i].name}" class="marker">${positions[i].name}</div>`, // 인포윈도우에 표시할 내용
+  // 마커에 표시할 커스텀 오버레이 생성.
+  let customOverlay = new kakao.maps.CustomOverlay({
+    position: position,
+    content: `<div id="${positions[i].name}" class="marker"> <span class="title">${positions[i].name}</span> </div>`,
+    yAnchor: 1,
   });
+  customOverlays.push(customOverlay);
 
-  // 마커에 mouseover 이벤트와 mouseout 이벤트를 등록합니다
-  // 이벤트 리스너로는 클로저를 만들어 등록합니다
-  // for문에서 클로저를 만들어 주지 않으면 마지막 마커에만 이벤트가 등록됩니다
-  kakao.maps.event.addListener(
-    marker,
-    "click",
-    makeClickListener(map, marker, infowindow)
-  );
-  // kakao.maps.event.addListener(marker, "mouseout", makeOutListener(infowindow));
+  // 마커 클릭 이벤트
+  kakao.maps.event.addListener(marker, "click", () => {
+    // 커스텀 오버레이를 전부 닫고
+    customOverlays.forEach((element) => {
+      element.setMap(null);
+    });
+
+    // Todo : 선택한 마커의 글 리스트를 보여주는 부분을 추가해줘야 됨.
+    const sideCon = document.querySelector(".side_right");
+    const sideList = document.querySelector(".right_list");
+    const titleLable = document.querySelector(".titleLable");
+    titleLable.textContent = marker.getTitle();
+
+    sideList.appendChild(titleLable);
+    sideList.classList.add("right_showit");
+    sideCon.appendChild(sideList);
+    // 선택한 오버레이를 열어준다.
+    customOverlay.setMap(map);
+  });
 }
-function makeClickListener(map, marker, infowindow) {
-  return () => {
-    infowindow.open(map, marker);
-    console.log(marker.getTitle());
-  };
-}
-// // 인포윈도우를 표시하는 클로저를 만드는 함수입니다
-// function makeOverListener(map, marker, infowindow) {
-//   return function () {
-//     infowindow.open(map, marker);
-//     console.log("클릭함");
-//   };
-// }
-
-// // 인포윈도우를 닫는 클로저를 만드는 함수입니다
-// function makeOutListener(infowindow) {
-//   return function () {
-//     infowindow.close();
-//   };
-// }
-// kakao.maps.event.addListener(marker, "click", function () {
-//   // 마커 위에 인포윈도우를 표시합니다
-//   console.log("클릭함");
-// });
-
 /*
-    마커 관련 부분 끝
-*/
+  마커 관련 부분 끝
+*****************************************/
