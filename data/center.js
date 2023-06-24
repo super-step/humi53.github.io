e1f5833dd8cf50b4172e984cbfe875fe9a8f60e5
humi53 <j.k.humi9@gmail.com>
2023-06-24 09:39:37 +0900

    메인지도합치기

---
+++ data/center.js
@@ -0,0 +1,17 @@
+const arrCenter = [
+  {
+    name: "gj",
+    Latitude: 35.16,
+    longitude: 126.8512,
+    zoomLv: 1,
+  },
+];
+
+// const arrCenter = {
+//   gj: {
+//     name: "gj",
+//     Latitude: 35.16,
+//     longitude: 126.8512,
+//     zoomLv: 1,
+//   },
+// };
